33d01c97809519638ccc922c0a2cbc0c0c516c16
Omar Laurino <olaurino@cfa.harvard.edu>
2015-07-02 17:46:48 -0400

    basic desktop with Groovy Console [skip ci]

--- src/main/java/cfa/vo/App.java
+++ src/main/java/cfa/vo/App.java
@@ -1,14 +1,63 @@
 package cfa.vo;
 
+import cfa.vo.iris.AbstractIrisApplication;
+import cfa.vo.iris.IrisComponent;
+
+import javax.swing.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends AbstractIrisApplication
 {
-    public App() {}
-    
-    public void getMessage() {
-        System.out.println( "Hello World!" );
+    private List<IrisComponent> components = new ArrayList();
+
+    public static void main( String[] args )
+    {
+        launch(App.class, args);
+    }
+
+    @Override
+    public String getName() {
+        return "SandBox";
+    }
+
+    @Override
+    public String getDescription() {
+        return "SandBox description";
+    }
+
+    @Override
+    public URL getSAMPIcon() {
+        return getClass().getResource("/iris_button_tiny.png");
+    }
+
+    @Override
+    public List<IrisComponent> getComponents() throws Exception {
+        return components;
+    }
+
+    @Override
+    public JDialog getAboutBox() {
+        return null;
+    }
+
+    @Override
+    public URL getDesktopIcon() {
+        return getClass().getResource("/Iris_logo.png");
+    }
+
+    @Override
+    public void setProperties(List<String> strings) {
+        components.add(new GroovyConsoleComponent());
+    }
+
+    @Override
+    public URL getHelpURL() {
+        return null;
     }
 }
